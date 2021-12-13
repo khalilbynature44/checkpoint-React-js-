135885c4fdc75b3e08238f3080dc117b4932951f
khalilbynature44 <93201829+khalilbynature44@users.noreply.github.com>
2021-12-13 10:21:50 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import CardList from "./CardList";
+function App() {
+  return (
+    <div>
+      <CardList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/CCard.js
@@ -0,0 +1,22 @@
+import { Card, Button } from "react-bootstrap";
+
+function CCard(props) {
+  return (
+    <div>
+      {" "}
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="imageInPublic.jpg" style= {{width:230}} />
+        <Card.Body>
+          <Card.Title>{props.title}</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
+
+export default CCard;
